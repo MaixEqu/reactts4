f28a271cbbe931bc0b6377a17bd5fd8b585b8e88
Debian live <eee1@i.ua>
2019-02-08 16:58:14 +0200

    test input added, ver 0.1.8 (J208)

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import * as mx from './mxlibcut';
 
-class App extends Component {
+export class App extends Component {
   render() {
-    let ver = "ver 0.1.7 (J208)";
+    let ver = "ver 0.1.8 (J208)";
     let ss = `[${mx.sFTime().trim()}]: ${ver}`;
    
     console.log(`hot-edit test ${ss}...`)
-//    <!-- img src={logo} className="App-logo" alt="logo" />
     return (
       <div className="App">
         <header className="App-header">
-          <div id="app"></div>
+          <div id="input"></div>
+          <div id="textareas"></div>
           <div className="VerInfo">{ver}</div>
         </header>
         <footer>{ss}</footer>
@@ -20,5 +20,39 @@ class App extends Component {
     );
   }
 }
+export class Textareas extends React.Component {
+    render() {
+      return (
+        <div>
+            <textarea cols={45} rows={25} defaultValue="textarea-1" />
+            <span> </span>
+            <textarea cols={45} rows={25} defaultValue="textarea-2" />
+        </div>
+      );
+    }
+}
 
-export default App;
+export class Input extends React.Component {
+    onChange = (event:any): void => {
+        console.log(mx.sFTime())
+        //this.setState({typed: event.target.value});
+    };
+    render() {
+        return (
+            <div>
+                <input type="text" onChange={this.onChange.bind(this)}/><br/>
+                You typed: <code>ddd</code>
+            </div>
+        );
+            /*
+      return (
+        <div>
+            <input type="text" onChange={this.onChange.bind(this)}/>
+            You typed: <code>{this.state}</code>
+        </div>
+      );
+      */
+    }
+}
+
+  
--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import * as App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-class Textareas extends React.Component {
-    render() {
-      return (
-        <div>
-            <textarea cols={45} rows={55}>textarea 1</textarea>
-            <span> </span>
-            <textarea cols={45} rows={55}>textarea 2</textarea>
-        </div>
-      );
-    }
-}
-  
-ReactDOM.render(
-  <Textareas />,
-  document.getElementById('app')
-);
+ReactDOM.render(<App.App />, document.getElementById('root'));
+ReactDOM.render(<App.Textareas />, document.getElementById('textareas'));
+ReactDOM.render(<App.Input/>, document.getElementById('input'));
 
