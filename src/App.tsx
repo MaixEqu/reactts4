ec5382e8516004e16a02e7aff3abc659844a30a6
PC home 1 <eee1@i.ua>
2019-02-10 01:12:27 +0200

    two input fields test, ver 0.1.13 (J209)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import * as mx from './mxlibcut';
 
-const sVersion = "ver 0.1.12 (J209)";
+const sVersion = "ver 0.1.13 (J209)";
 
 export class App extends Component {
   render() {
@@ -11,6 +11,7 @@ export class App extends Component {
       <div className="App">
         <header className="App-header">
           <div id="input"></div>
+          <div id="button"></div>
           <div id="textareas"></div>
         </header>
         <footer>{time_ver}</footer>
@@ -31,9 +32,9 @@ export class Textareas extends React.Component {
   render() {
     return (
       <div>
-        <textarea cols={25} rows={15} onChange={this.onChange2.bind(this)} defaultValue="textarea-1" />
+        <textarea name="t1" cols={25} rows={15} onChange={this.onChange1.bind(this)} defaultValue="textarea-1" />
         <span> </span>
-        <textarea cols={25} rows={15} onChange={this.onChange2.bind(this)} defaultValue="textarea-2" />
+        <textarea name="t2" cols={25} rows={15} onChange={this.onChange2.bind(this)} defaultValue="textarea-2" />
       </div>
     );
   }
@@ -43,12 +44,88 @@ export class Input extends React.Component {
   onChange = (event: React.ChangeEvent<HTMLInputElement>):void => {
     console.log("valueI: ", event.target.value);
     console.log("defaultValueI: ", event.target.defaultValue);
+    this.set(mx.sFTime(), event);
+    console.log("valueI new: ", event.target.value);
+  }
+  set = (value: string, event: React.ChangeEvent<HTMLInputElement>) => {
+    event.target.value = value;
   }
   render() {
     return (
       <div>
-        <input type="text" onChange={this.onChange.bind(this)} defaultValue="input field 75" /><br/>
+        <input type="text" onChange={this.onChange.bind(this)} defaultValue="input field 73" /><br/>
       </div>
     );
   }
 }
+
+interface IState {
+  input: string;
+  input1: string;
+  input2: string;
+}
+interface IProps {}
+export class Button extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+        input: 'Play 8',
+        input1: '1',
+        input2: '2'
+    };
+  }
+  //state: IState = { input: "dfsf" };
+  getState(): string { return this.state.input || "" };
+  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ input: e.target.value });
+    console.log(this.state.input + " >> " + mx.sFTime());
+  }
+  handleChange1 = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ input1: e.target.value });
+    console.log(mx.sFTime()+". 1:"+this.state.input1+", 2:"+this.state.input2);
+  }
+  handleChange2 = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ input2: e.target.value });
+    console.log(mx.sFTime()+". 1:"+this.state.input1+", 2:"+this.state.input2);
+  }
+
+  handleClick(e: React.MouseEvent<HTMLInputElement>) {
+    console.log(this.getState() + mx.sFTime());
+  }
+
+  render() {
+    return (
+      <div>
+        <input name="mxinput1" type="text" onChange={ this.handleChange1 } />
+        <input name="mxinput2" type="text" onChange={ this.handleChange2 } />
+        <input
+          type="button"
+          name="mxbutton"
+          value="Text input 10"
+          onClick={ this.handleClick }
+        />
+      </div>
+    );
+  }
+}
+export class Button2 extends React.Component {
+  handleClick() {
+    if (this.refs.myInput !== null) {
+    	var input: any = this.refs.myInput;
+			var inputValue = input.value;
+      alert("Input is " + inputValue);
+    }
+  }
+  render() {
+    return (
+      <div>
+        <input type="text" ref="myInput" />
+        <input
+          type="button"
+          value="Text input 2"
+          onClick={this.handleClick}
+        />
+      </div>
+    );
+  }
+};
--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,6 @@ import * as App from './App';
 
 ReactDOM.render(<App.App />, document.getElementById('root'));
 ReactDOM.render(<App.Textareas />, document.getElementById('textareas'));
-ReactDOM.render(<App.Input/>, document.getElementById('input'));
+ReactDOM.render(<App.Input />, document.getElementById('input'));
+ReactDOM.render(<App.Button />, document.getElementById('button'));
 
