3f03c182146f33e38badddb6540decc286ca2aa0
MaixEqu <36164811+MaixEqu@users.noreply.github.com>
2019-02-09 10:55:17 +0200

    input OnChange test, ver 0.1.10 (J209)

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React, { Component, ChangeEventHandler } from 'react';
 //import './App.css';
 import * as mx from './mxlibcut';
 
+const sVersion = "ver 0.1.10 (J209)";
+
 export class App extends Component {
   render() {
-    let ver = "ver 0.1.9 (J208)";
-    let time_ver = `[${mx.sFTime().trim()}]: ${ver}`;
+    let time_ver = `[${mx.sFTime().trim()}]: ${sVersion}`;
    
     console.log(`hot-edit textareas tests ${time_ver}...`)
     return (
@@ -34,16 +35,23 @@ export class Textareas extends React.Component {
 }
 
 export class Input extends React.Component {
+    log = (event: React.ChangeEvent<HTMLInputElement>):void => {
+        console.log("value: ", event.target.value);
+        console.log("defaultValue: ", event.target.defaultValue);
+    }
+
     onChange = (event: React.ChangeEvent<HTMLInputElement> ): void => {
         console.log("v6 " + mx.sFTime() + this)
         //this.setState({typed: event.target.value});
-    };
+    }
+
     render() {
         return (
             <div>
-                <input type="text" onChange={this.onChange.bind(this)}/><br/>
+                <input type="text" onChange={this.log.bind(this)} defaultValue="input field 78" /><br/>
             </div>
         );
+                // <input type="text" onChange={this.onChange.bind(this)} defaultValue="input field 77" /><br/>
             /*
       return (
         <div>
