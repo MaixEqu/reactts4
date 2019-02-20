d69fae006c8fab9edd4f141aab38a65268eb4c3b
Debian live <eee1@i.ua>
2019-02-20 18:28:22 +0200

    files not read, ver 0.7.2 (J2K)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import * as mx from './mxlib';
 import * as ipa from './ipa.lib';
 
-const sVersion = "ver 0.7.1 (J2K)";
+const sVersion = "ver 0.7.2 (J2K)";
 
 export class Main extends Component {
   render() {
@@ -19,65 +19,48 @@ export class Main extends Component {
   }
 }
 
+const sGetDataX = async (path: string, callback: Function) => {
+  console.log("async X: " + path)
+  try {
+    const response = await fetch(path);
+    const text = await response.text();
+    //console.log("X: " + text);
+    //this.setState({text_s: text});
+    callback(path, text)
+  } catch(error) {
+    console.error(error);
+  }
+}
+
+let sFData = "no data";
 // =====================================================
 interface IState {
   text_s: string
   width_s?: string
   height_s?: string
+  stage_s?: number
 }
-
 interface IProps {
   text_p?: string;
   width_p?: string
   height_p?: string
-  datapath?: string
-  readonly?: boolean;
-  onTextChange?: (value: string, e: any) => void;
-  onMDown?: (e: any) => void;
 }
 
-const TAInfo = (state: IState) => {
-  return <p>Symbols in 1st textarea: [{state.text_s.length}]</p>;
-}
-
-const taNames: Map<string, string> = new Map([
-  ['txt', 'Text'],
-  ['res', 'Result'],
-]);
-
-class TextArea1 extends React.Component<IProps, IState> {
-  private _sData = "";
+export class TextAreas extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
     this.handleMDown = this.handleMDown.bind(this);
-    const sDataUrl = location.href + this.props.datapath; //'/data/5.txt'
-    console.log(sDataUrl)
-    //const sData = this.sGetData(sDataUrl);
-    this.sGetData(sDataUrl);
-    //this.setState({'text': sData})
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.state = {text_s: "'. // hello to Mx\n", height_s: '300'};
   }
 
-  sGetData = (path: string): void => {
-    //let sRes = "no data";
-    fetch(path)
-      .then((response) => response.text())
-      .then((sRes) => {
-        console.log(sRes)
-        this.setState({text_s: sRes}); // ._sData = sRes
-      })
-      .catch((error: Error) => {
-        console.error(error);
-      });
-    //return sRes;
-  }
-
-  handleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
-    if (this.props.onTextChange) this.props.onTextChange(e.target.value, e);
+  handleTextChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    this.setState({text_s: e.target.value});
   }
   handleMDown(e: React.MouseEvent<HTMLTextAreaElement>) {
-    if (this.props.onMDown) this.props.onMDown(e);
+    // this.setState({height2: e.target.scrollHeight});
+    // this.setState({width2: e.target.scrollWidth});
   }
   handleKeyDown(e: any ) {
     // console.log("Kdown1, " + `${e.target.scrollHeight}px`);
@@ -86,59 +69,6 @@ class TextArea1 extends React.Component<IProps, IState> {
   }
 
   render() {
-    const text = this.props.text_p;
-    const taStyle = {
-      width: this.props.width_p+'px',
-      height: this.props.height_p+'px',
-      border: "2px solid", 
-      verticalAlign: "top",
-    };
-    return (
-        <textarea style={taStyle} className="halfsize" value={text}
-          onChange={this.handleChange}
-          onKeyDown={this.handleKeyDown}
-          onMouseUp={this.handleMDown}
-          readOnly={this.props.readonly}
-        />
-    );
-  }
-}
-
-//           onMouseDown={this.handleMDown}
-
-const sGetData2 = (path: string): string => {
-  let sRes = "no data";
-  fetch(path)
-    .then((response) => response.text())
-    .then((sRes) => {
-      return sRes;
-    })
-    .catch((error: Error) => {
-      console.error(error);
-    });
-  return sRes;
-}
-
-export class TextAreas extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-    this.handleTextChange = this.handleTextChange.bind(this);
-    this.handleMDown = this.handleMDown.bind(this);
-    this.state = {text_s: "'. // hello to Mx\n", height_s: '300'};
-  }
-  
-  handleTextChange(text: string, e: any) {
-    this.setState({text_s: text});
-    //this.setState({height2: e.target.scrollHeight});
-  }
-
-  handleMDown(e: any) {
-    // this.setState({height2: e.target.scrollHeight});
-    // this.setState({width2: e.target.scrollWidth});
-  }
-
-  render() {
-    const text = this.state.text_s;
     const heigth = this.state.height_s;
     const taStyle = {
       height: this.state.height_s+'px',
@@ -151,15 +81,17 @@ export class TextAreas extends React.Component<IProps, IState> {
         <fieldset>
           <legend>Enter text:</legend>
           <div style={{border: "0px solid", verticalAlign: "top"}}>
-            <TextArea1
-              text_p={text}
-              height_p={heigth}
-              onTextChange={this.handleTextChange}
-              datapath='data/5.txt'
-              onMDown ={this.handleMDown} />
-            {' '}
-            <textarea style={taStyle} className="halfsize" defaultValue={this.state.text_s}
-            />
+          <textarea style={taStyle} className="halfsize"
+            defaultValue={this.state.text_s}
+            onChange={this.handleTextChange}
+            onKeyDown={this.handleKeyDown}
+            onMouseUp={this.handleMDown}
+          />
+          {' '}
+          <textarea style={taStyle} className="halfsize" 
+            defaultValue={this.state.text_s}
+            readOnly={true}
+          />
           </div>
         </fieldset>
         <TAInfo text_s={this.state.text_s} />
@@ -167,11 +99,7 @@ export class TextAreas extends React.Component<IProps, IState> {
     );
   }
 }
-//               text={ipa.ipa2cyr(text)}
-/*
-            {' '}
-            <TextArea
-              text={sConvert(text, doUnderlines)}
-              height={heigth}
-              onMDown ={this.handleMDown} />
-*/
+
+const TAInfo = (state: IState) => {
+  return <p>2 Symbols in 1st textarea: [{state.text_s.length}]</p>;
+}
